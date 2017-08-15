16a4e018fea43b100e3887caa774e89cd83751bc
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-15 16:21:11 +0000

    Removed pg. Added moment.

--- pg-javascript-famous-people-knex/test_script.js
+++ pg-javascript-famous-people-knex/test_script.js
@@ -1,17 +1,5 @@
-const pg = require("pg")
+const moment = require('moment');
 const settings = require("./settings") // settings.json
-
-const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
-})
-
-
-
 const knex = require('knex')({
   client: 'pg',
   connection: {
@@ -26,10 +14,6 @@ const knex = require('knex')({
 
 
 
-client.connect((err) => {
-  if (err) {
-    return console.error("Database Connection Error", err)
-  }
 
   knex.select('*').from('famous_people')
     .then(function(rows) {
@@ -39,7 +23,7 @@ client.connect((err) => {
           let id    = result.id
           let fname = result.first_name
           let lname = result.last_name
-          let bdate = result.birthdate
+          let bdate = moment(result.birthdate).format('YYYY-MM-DD')
 
           console.log(`- ${id}: ${fname} ${lname}, born ${bdate}`)
 
@@ -47,11 +31,12 @@ client.connect((err) => {
       } else {
         console.log('No result found')
       }
-      client.end();
+      // knex.close()
+      // knex.disconnect()
     })
     .catch(function(error) {
-      client.end();
       console.error(error)
+      // knex.close()
+      // knex.disconnect()
     })
 
-})
