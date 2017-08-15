af343c6ce2c8e85a358c23f4d19e0d1209e22583
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-15 16:06:38 +0000

    Added knex.

--- pg-javascript-famous-people-knex/test_script.js
+++ pg-javascript-famous-people-knex/test_script.js
@@ -1,5 +1,5 @@
-const pg = require("pg");
-const settings = require("./settings"); // settings.json
+const pg = require("pg")
+const settings = require("./settings") // settings.json
 
 const client = new pg.Client({
   user     : settings.user,
@@ -10,31 +10,48 @@ const client = new pg.Client({
   ssl      : settings.ssl
 })
 
+
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host     : settings.hostname,
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database
+  }
+})
+
+
+
+
+
 client.connect((err) => {
   if (err) {
     return console.error("Database Connection Error", err)
   }
 
-  let query = `SELECT * FROM famous_people
-               WHERE first_name = $1 OR last_name = $1`
-  client.query(query, [process.argv[2]], (err, result) => {
-    if (err) {
-      return console.error("error running query", err)
-    }
-    result = result.rows
-    if (result.length !== 0){
-      for (let index in result) {
-        let id    = result[index].id
-        let fname = result[index].first_name
-        let lname = result[index].last_name
-        let bdate = result[index].birthdate
+  knex.select('*').from('famous_people')
+    .then(function(rows) {
+      if (rows !== 0){
+        for (let index in rows) {
+          let result = rows[index]
+          let id    = result.id
+          let fname = result.first_name
+          let lname = result.last_name
+          let bdate = result.birthdate
 
-        console.log(`- ${id}: ${fname} ${lname}, born ${bdate}`)
+          console.log(`- ${id}: ${fname} ${lname}, born ${bdate}`)
 
+        }
+      } else {
+        console.log('No result found')
       }
-    } else {
-      console.log('No result found')
-    }
-    client.end();
-  })
+      client.end();
+    })
+    .catch(function(error) {
+      client.end();
+      console.error(error)
+    })
+
 })
