d754298f59ea84b874a71949a7f6d271dd715bc5
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-15 17:44:20 +0000

    Refactored the code.

--- pg-javascript-famous-people/test_script_refactored.js
+++ pg-javascript-famous-people/test_script_refactored.js
@@ -1,6 +1,7 @@
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
 
+
 const client = new pg.Client({
   user     : settings.user,
   password : settings.password,
@@ -9,6 +10,7 @@ const client = new pg.Client({
   port     : settings.port,
   ssl      : settings.ssl
 })
+
 
 client.connect((err) => {
   if (err) {
